fa1c2cc5fccaa73c54099dfe5dc98af97b927f17
Jed Fox <git@jedfox.com>
2022-01-14 08:18:21 -0500

    s/ts/babel/

--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ const { readFileSync } = require("node:fs");
 const fs = require("node:fs/promises");
 const Module = require("node:module");
 
-const ts = require("typescript");
+const { transformFileSync } = require("@babel/core");
+
 /** @type {any} */
 const { absolutePath } = require("@11ty/eleventy/src/TemplatePath");
 
@@ -13,77 +14,41 @@ const { createElement } = require("./jsx");
 const AsyncFunction = (async () => {}).constructor;
 
 /** @typedef {{default: (data: unknown) => import('hast').RootContent | import('hast').RootContent[], data?: unknown}} Instance */
-/** @typedef {{typescript?: import('typescript').CompilerOptions, toHtml?: import('hast-util-to-html').Options }} Options */
+/** @typedef {{babel?: import('@babel/core').TransformOptions, toHtml?: import('hast-util-to-html').Options }} Options */
 
 /** @type {(eleventyConfig: import("@11ty/eleventy/src/UserConfig"), opts: Options) => void} */
-exports.plugin = (eleventyConfig, { typescript, toHtml } = {}) => {
+exports.plugin = (eleventyConfig, { babel: babelOptions, toHtml } = {}) => {
   eleventyConfig.addTemplateFormats(["tsx", "jsx"]);
 
-  /** @type {ts.CompilerOptions} */
-  const tsOptions = {
-    jsx: ts.JsxEmit.React,
-    jsxFactory: "createElement",
-    jsxFragmentFactory: "createElement.Fragment",
-    module: ts.ModuleKind.CommonJS,
-    moduleResolution: ts.ModuleResolutionKind.NodeNext,
-    esModuleInterop: true,
-    ...typescript,
+  const babelOpts = {
+    babelrc: false,
+    ...babelOptions,
+    plugins: [
+      "@babel/plugin-syntax-jsx",
+      [
+        "@babel/plugin-transform-react-jsx",
+        {
+          runtime: "classic",
+          pragma: "createElement",
+          pragmaFrag: "createElement.Fragment",
+          useSpread: true,
+        },
+      ],
+      ...(babelOptions?.plugins ?? []),
+    ],
   };
+
+  require("@babel/register")(babelOpts);
 
   /** @type {Map<string, Instance>} */
   const instances = new Map();
-
-  /**
-   * @param {string} fileName
-   * @param {Module} module
-   * @returns {NodeRequire}
-   */
-  function createRequire(fileName, module) {
-    const require = Module.createRequire(fileName);
-    return Object.assign(
-      (/** @type {string} */ name) =>
-        loadModule(require.resolve(name), module, require),
-      require
-    );
-  }
-
-  /**
-   * @param {string} absPath
-   * @param {Module} parent
-   * @param {NodeRequire} require
-   */
-  function loadModule(absPath, parent, require) {
-    if (!absPath.endsWith(".jsx")) return require(absPath);
-    const childModule = new Module(absPath, parent);
-    childModule.filename = absPath;
-
-    const input = readFileSync(absPath, "utf-8");
-    const transpiledSource = ts.transpile(input, tsOptions, absPath);
-
-    Function(
-      "module",
-      "exports",
-      "require",
-      "createElement",
-      transpiledSource
-    )(
-      childModule,
-      childModule.exports,
-      createRequire(absPath, childModule),
-      createElement
-    );
-
-    return childModule.exports;
-  }
-
   /** @type {(_: string) => Promise<Instance>} */
   async function createInstance(inputPath) {
     const fileName = absolutePath(inputPath);
     const childModule = new Module(fileName, /** @type {any} */ module);
     childModule.filename = fileName;
 
-    const input = await fs.readFile(fileName, "utf-8");
-    const transpiledSource = ts.transpile(input, tsOptions, fileName);
+    const transpiledSource = transformFileSync(fileName, babelOpts);
 
     await AsyncFunction(
       "module",
@@ -94,7 +59,7 @@ exports.plugin = (eleventyConfig, { typescript, toHtml } = {}) => {
     )(
       childModule,
       childModule.exports,
-      createRequire(fileName, childModule),
+      Module.createRequire(fileName),
       createElement
     );
 
