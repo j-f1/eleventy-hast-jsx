0dc2edbc9b0b13d486e6fffc3e33aa851d7f0873
Jed Fox <git@jedfox.com>
2022-03-13 14:26:27 -0400

    fix processChildren

--- src/process-children.js
+++ src/process-children.js
@@ -2,17 +2,17 @@
  * @param {any[]} children
  * @returns {import("hast").Node[]}
  */
-module.exports = (children) =>
-  Array.isArray(children)
-    ? children
-        .flat(20)
-        .flatMap((ch) =>
-          typeof ch === "string"
-            ? [{ type: "text", value: ch }]
-            : typeof ch === "number"
-            ? [{ type: "text", value: ch.toString() }]
-            : ch == null || typeof ch === "boolean"
-            ? []
-            : [ch]
-        )
-    : [children];
+const processChildren = (children) =>
+  [children]
+    .flat(21)
+    .flatMap((ch) =>
+      typeof ch === "string"
+        ? [{ type: "text", value: ch }]
+        : typeof ch === "number"
+        ? [{ type: "text", value: ch.toString() }]
+        : ch == null || typeof ch === "boolean"
+        ? []
+        : [ch]
+    );
+
+module.exports = processChildren;
